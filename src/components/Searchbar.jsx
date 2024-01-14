dcd4dbe6d4aa8bddc133ee119406e3d09d3c81eb
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-14 18:55:25 +0200

    searchbar

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export class SearchBar extends Component {
+  state = {
+    imageName: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ imageName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.imageName.trim() === '') {
+      alert('Введіть дані для пошуку');
+      return;
+    }
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: '' });
+  };
+
+  render() {
+    return (
+      <header>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            type="text"
+            name="imageName"
+            value={this.state.imageName}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
