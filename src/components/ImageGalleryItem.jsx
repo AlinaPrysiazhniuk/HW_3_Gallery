1a58ae722f64708971cb1acc5c2d052b81547677
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-15 15:40:08 +0200

    list

--- src/components/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem.jsx
@@ -4,6 +4,13 @@ export const ImageGalleryItem = ({ image }) => {
       <p>Total: {image.total}</p>
       <p>TotalHits: {image.totalHits}</p>
       <p>TotalHits: {image.hits.length}</p>
+      <ul>
+        {image.hits.map(hits => (
+          <li key={hits.id}>
+            <img src={hits.webformatURL} alt="" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
